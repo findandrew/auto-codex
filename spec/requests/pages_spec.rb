04e150b1b1779f7b54394d38f74084b276f42a7d
Andrew Miller <mail@andrewjm.co>
2026-02-23 20:03:59 -0500

    Add visible preview verification copy

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -5,6 +5,6 @@ RSpec.describe "Pages", type: :request do
     get root_path
 
     expect(response).to have_http_status(:ok)
-    expect(response.body).to include("Hello, world! Welcome to AutoCodex. Hope you are having a nice day :)")
+    expect(response.body).to include("Hello, world! Welcome to AutoCodex. Hope you are having a nice day :) Preview verification branch message.")
   end
 end
